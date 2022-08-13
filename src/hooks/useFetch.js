53005cfdeac4e63214fda59e1888cd8d05c3a7ed
ahmedSadoun <ahmedsaadoun866@gmail.com>
2022-08-13 23:51:24 +0200

    handling error of a request in case of there is an error in the url

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -2,14 +2,29 @@ import React, { useState, useEffect } from "react";
 
 export default function useFetch(url) {
     const [data, setData] = useState(null);
+    const [isPending ,setIsPending]=useState(true)
+    const [error, setError] = useState('');
     //useEffect is used to run a specific code once based on a specific condition
     useEffect(() => {
       const fetchTrips =async ()=>{
-            const response = await fetch(url)
-             const json= await response.json();
-             setData(json);
+        try{
+
+          const response = await fetch(url)
+          if(!response.ok){
+            throw new Error(response.statusText)
+          }
+          const json= await response.json();
+          setData(json);
+          setIsPending(false)
+          setError(null)
+
+        }catch (error){
+          console.log(error)
+          setError('Could not fetch the data ' )
+          setIsPending(false)
+        }
       };
       fetchTrips();
     }, [url ]);
   
-    return{data : data} }
+    return{data : data ,isPending , error} }
