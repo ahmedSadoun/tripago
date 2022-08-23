ccc0e4aa98f93bd000b43513dbaaf9c53cf11a74
ahmedSadoun <ahmedsaadoun866@gmail.com>
2022-08-23 19:13:10 +0200

    useEffect cleanup function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import { useState } from 'react';
 import Triplist from './components/TripList'
 function App() {
+ const  [showTrips,setShowTrips]=useState(true)
+  const onClickHandler = ()=>{
+    
+    setShowTrips(!showTrips)
+  }
   return (
     <div >
-     <Triplist/>
+      <button onClick={()=>onClickHandler()}>Hide Trips</button>
+     {showTrips && <Triplist/>}
     </div>
   );
 }
